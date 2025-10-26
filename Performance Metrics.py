e4cb8164d82902d2900c588fc2492ee2e99b8f0b
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-26 23:48:42 +0800

    Update Performance Metrics.py

--- Performance Metrics.py
+++ Performance Metrics.py
@@ -106,11 +106,11 @@ if not Business_Administration_df.empty and all(col in Business_Administration_d
         split=True,
         inner='quartile' # Adds lines for quartile and median
 )
-        plt.title('Overall GPA Distribution by Job Status and Gender', fontsize=14)
-        plt.xlabel('Has a Job', fontsize=12)
-        plt.ylabel('Overall GPA', fontsize=12)
-        plt.legend(title='Gender')
-        plt.grid(axis='y', linestyle='--', alpha=0.7)
-        plt.tight_layout()
-        st.pyplot(fig, use_container_width=True)
+    plt.title('Overall GPA Distribution by Job Status and Gender', fontsize=14)
+    plt.xlabel('Has a Job', fontsize=12)
+    plt.ylabel('Overall GPA', fontsize=12)
+    plt.legend(title='Gender')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    st.pyplot(fig, use_container_width=True)
 
