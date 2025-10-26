993f8b52efbd0a5d4ad2cfa6c9a51c32dc58d97d
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-26 19:43:46 +0800

    Update Performance Metrics.py

--- Performance Metrics.py
+++ Performance Metrics.py
@@ -67,7 +67,7 @@ if 'Job' in Business_Administration_df.columns and 'Overall' in Business_Adminis
         with col2:
             st.subheader("3. Overall GPA by Job Status and Gender")
             
-    fig,ax=plt.subplots(figsize=(10, 6))
+    fig, ax= plt.subplots(figsize=(10, 6))
 
     sns.violinplot(
        x='Job',
