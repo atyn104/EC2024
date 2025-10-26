7e1b6905683acb535f5e451b69670ee217c90c0a
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-26 16:54:39 +0800

    Update Performance Metrics.py

--- Performance Metrics.py
+++ Performance Metrics.py
@@ -1,1 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
 
+# URL for the data file
+url = "https://raw.githubusercontent.com/atyn104/EC2024/refs/heads/main/Business_Administration_Department_data.csv"
+
+# Set page configuration
+st.set_page_config(
+    page_title="Scientific Visualization",
+    layout="wide"
+)
+
+st.title("Distribution of Gender in Arts Faculty")
+st.markdown("---")
+
+# Function to load data with caching
+@st.cache_data
+def load_data(data_url):
+    try:
+        df = pd.read_csv(url)
+        return df
+    except Exception as e:
+        st.error(f"An error occurred while loading the file from the URL: {e}")
+        return pd.Dataframe()
+
+arts_df = load_data(url)
+
+if arts_df.empty:
+    st.stop()
+    
+st.subheader("1.Raw Data Preview")
+st.dataframe(arts_df.head(), use_container_width=True)
