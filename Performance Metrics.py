742c47fe257082a49feed1125eefb34bf0f1fe20
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-28 00:41:40 +0800

    Update Performance Metrics.py

--- Performance Metrics.py
+++ Performance Metrics.py
@@ -105,7 +105,7 @@ if not Business_Administration_df.empty and all(col in Business_Administration_d
                 palette={'Male': 'skyblue', 'Female': 'lightcoral'},
                 split=True,
                 inner='quartile',# Adds lines for quartile and median
-                ax = ax
+                ax=ax
         )
             ax.set_title('Overall GPA Distribution by Job Status and Gender', fontsize=14)
             ax.set_xlabel('Has a Job', fontsize=12)
