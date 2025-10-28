1f7a6e0e4d31b0ae4a4d243facbcde603a56433b
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-28 23:30:09 +0800

    Update Performance Metrics.py

--- Performance Metrics.py
+++ Performance Metrics.py
@@ -4,6 +4,13 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+col1, col2, col3, col4 = st.columns(4)
+   
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 # URL for the data file
 url = "https://raw.githubusercontent.com/atyn104/EC2024/refs/heads/main/Business_Administration_Department_data.csv"
 
@@ -16,12 +23,6 @@ st.set_page_config(
 st.title("Student Performance Metrics ")
 st.markdown("---")
 
-col1, col2, col3, col4 = st.columns(4)
-   
-col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
-col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
-col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
 
 # Function to load data with caching
 @st.cache_data
