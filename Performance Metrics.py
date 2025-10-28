3dc309f40c1aac2184f63d1ac8ba357e85943b8b
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-28 23:36:02 +0800

    Update Performance Metrics.py

--- Performance Metrics.py
+++ Performance Metrics.py
@@ -16,6 +16,14 @@ st.set_page_config(
 st.title("Student Performance Metrics ")
 st.markdown("---")
 
+col1, col2, col3, col4 = st.columns(4)
+   
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+st.markdown("---")
+
 
 # Function to load data with caching
 @st.cache_data
@@ -31,14 +39,6 @@ Business_Administration_df = load_data(url)
 
 if Business_Administration_df.empty:
     st.stop()
-
-col1, col2, col3, col4 = st.columns(4)
-   
-col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
-col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
-col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
-st.markdown("---")
     
 st.subheader("1. Raw Data Preview")
 st.dataframe(Business_Administration_df.head(), use_container_width=True)
