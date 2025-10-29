d289fc93e1b9f84786ec8e2cd43edc8ab7120bb0
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-29 20:57:24 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 st.set_page_config(page_title="Student Performance Metrics")
 
-# Page definitions
+# --- Page definitions ---
 home = st.Page("home.py", title="Homepage", icon=":material/home:", default=True)
 objective2 = st.Page("Performance Metrics.py", title="Objective Number 2", icon=":material/analytics:")
 
-# Navigation menu
+# --- Navigation menu ---
 pg = st.navigation({
     "Menu": [home],
+    "📊 This page visualizes graphs based on objectives": [],
     "🎓 Student Performance Metrics": [objective2],
 })
 
-# Run the selected page
 pg.run()
