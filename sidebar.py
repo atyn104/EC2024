471a330439d8e2286de2e96a0074eaf8dd8ac974
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-30 00:35:18 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Student Performance Metrics")
 
 # --- Page definitions ---
 home = st.Page("home.py", title="Homepage", icon=":material/home:", default=True)
-performance = st.Page("Performance Metrics.py", title=" Objective Number 1 ", icon=":material/analytics:")
+performance = st.Page("Performance Metrics.py", title=" Objective Number 1 ", icon=":material/insights:")
 objective2 = st.Page("obj number 2.py", title=" Objective Number 2 ", icon=":material/insights:")
 objective3 = st.Page("obj number 3.py", title=" Objective Number 3 ", icon=":material/insights:")
 
