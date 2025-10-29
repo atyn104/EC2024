cff1308bebcd7fbc73dfc8374d9076279e58be0d
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-29 21:14:43 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -9,8 +9,7 @@ objective2 = st.Page("Performance Metrics.py", title="Objective Number 2", icon=
 # --- Navigation menu (pages setup) ---
 pg = st.navigation({
     "Menu": [home],
-    "🎓 Student Performance Metrics",
-    [objective2],
+    "🎓 Student Performance Metrics": [objective2],
 })
 # --- Run selected page ---
 pg.run()
