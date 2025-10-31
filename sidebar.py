d7b3a472fd475e73e5c1bdb2b3ebb4d6b9cac601
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-31 20:57:12 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,33 +1,17 @@
 import streamlit as st
 
-# Set the page config
-st.set_page_config(page_title="Student Performance Metrics")
+st.set_page_config(
+    page_title="Student Performance Metrics "
+)
 
-# --- Page definitions ---
-# PASTIKAN Anda menyertakan "pages/" di sini
-home = "home.py"
-performance = "Performance Metrics.py"
-objective2 = "obj number 2.py"
-objective3 = "obj number 3.py"
+visualise = st.Page('Performance Metrics.py', title='Student Performance Metrics', icon=":material/school:")
 
-# --- Navigation menu ---
-page = st.sidebar.radio("Navigate", 
-                       ["Homepage", "Objective Number 1", "Objective Number 2", "Objective Number 3"])
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
-# --- Run the selected page ---
-if page == "Homepage":
-    (open('home.py').read())
-    # You can import and display the contents of home.py here, like:
-    # exec(open('pages/home.py').read())
+pg = st.navigation(
+        {
+            "Menu": [home, visualise]
+        }
+    )
 
-elif page == "Objective Number 1":
-    st.write(f"Loading {performance}...")
-    # exec(open('pages/Performance Metrics.py').read())
-
-elif page == "Objective Number 2":
-    st.write(f"Loading {objective2}...")
-    # exec(open('pages/obj number 2.py').read())
-
-elif page == "Objective Number 3":
-    st.write(f"Loading {objective3}...")
-    # exec(open('pages/obj number 3.py').read())
+pg.run()
