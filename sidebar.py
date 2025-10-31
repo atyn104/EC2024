400d53674dfc7ded630a50a4baeb98e1358c8150
atyn104 <s22a0014@siswa.umk.edu.my>
2025-10-31 20:54:33 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -16,8 +16,8 @@ page = st.sidebar.radio("Navigate",
 
 # --- Run the selected page ---
 if page == "Homepage":
-    st.write(f"Loading {home}...")
     (open('home.py').read())
+    st.write(f"Loading {home}...")
     # You can import and display the contents of home.py here, like:
     # exec(open('pages/home.py').read())
 
